963356c2a54499ca83bde6d5c2011c3c6a8d327f
Abhay Kumar <shindheabhay@gmail.com>
2024-11-04 15:43:43 +0530

    footer button added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2881,6 +2881,19 @@ export default function Home() {
           </div>
         </div>
       )}
+      
+      {/* mobile sticky bar */}
+
+      <div className=' relative w-full'>
+        <div className='flex justify-center items-center sm:gap-4 gap-2 sm:px-0 px-2 fixed bottom-0 z-40 bg-white lg:hidden text-white sm:text-sm text-xs font-medium h-[84px] w-full'>
+          <div className='w-1/2 sm:w-2/5 bg-[#033E4D] rounded-lg shadow-lg h-11 flex justify-center items-center'>
+          BOOK NOW AT RS 299 
+          </div>
+          <div className='w-1/2 sm:w-2/5 bg-[#38B557] rounded-lg shadow-lg flex h-11 justify-center items-center'>
+          BOOK CONSULTATION
+          </div>
+        </div>
+      </div>
     </>
   );
 }
